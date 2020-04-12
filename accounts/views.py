1d1bdd93ad08c545795c1bc4d8697b78f13b0ffa
Pmparekh2000 <pmp.tech2000@gmail.com>
2020-04-12 13:50:30 +0530

    Basic urls configured

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse('home page')
+
+def products(request):
+    return HttpResponse('contact page')
+
+def customer(request):
+    return HttpResponse('contact page')    
 # Create your views here.
